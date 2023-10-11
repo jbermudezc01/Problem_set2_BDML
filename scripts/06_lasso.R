05222c3e9b88a23fae6115d9dff1d5f7835c4d25
esmesago <142452751+esmesago@users.noreply.github.com>
2023-10-11 17:43:37 -0500

    update 06.lasso

--- scripts/06_lasso.R
+++ scripts/06_lasso.R
@@ -1,7 +1,5 @@
 ### modelo de predicción: lasso  ##### 
 
-## limpieza y transformación de datos ##
-
 #Limpieza area de trabajo 
 rm(list=ls())
 cat('\014')
@@ -68,14 +66,32 @@ lasso_workflow <- workflow() %>%
   add_recipe(receta) %>%
   add_model(lasso_spec)
 
-
 # realizar busqueda de hiperparametros utilizando validación cruzada 
 
-# definir las particiones 
+# definimos el data set train como  sf
+train_sf <- st_as_sf( 
+  train,
+  coords = c("lon", "lat"), 
+  crs = 4326
+)
+
+# realizar una validación cruzada de bloques espaciales
+
+set.seed(123)
+block_folds <- spatial_block_cv(train_sf, v = 5) 
+
+# visualización de plieges 
+autoplot(block_folds) # visualización de los plieges 
+
+# excluimos una zona (a), entrenamos las demás ( b,c,d,e) y validamos con (a) 
+
+walk(block_folds$splits, function(x) print(autoplot(x)))
+
+# Crear pliegues para la validación cruzada
 
 df_fold <- vfold_cv(train, v = 5)
 
-# realizar bsuqeuda de hiperparametro 
+# busqueda de hiperparametros 
 
 tune_lasso <- tune_grid(
   lasso_workflow,
@@ -83,99 +99,30 @@ tune_lasso <- tune_grid(
   grid = penalty_grid,
   metrics = metric_set(rmse)
 )
-
-# graficar resultados de busqueda de hiperparametros
-
-### modelo de predicción: lasso  ##### 
-
-## limpieza y transformación de datos ##
-
-#Limpieza area de trabajo 
-rm(list=ls())
-cat('\014')
-
-# cargar paquetes 
-install.packages("pacman")
-library(pacman)
-# cargar librerias 
-p_load(tidyverse, # Manipular dataframes
-       rio, # Importar datos fácilmente
-       plotly, # Gráficos interactivos
-       leaflet, # Mapas interactivos
-       rgeos, # Calcular centroides de un polígono
-       units, # unidades
-       sf, # Leer/escribir/manipular datos espaciales
-       osmdata, # Obtener datos de OpenStreetMap (OSM)
-       tidymodels, # Modelado de datos limpios y ordenados
-       randomForest, # Modelos de bosque aleatorio
-       rattle, # Interfaz gráfica para el modelado de datos
-       spatialsample,
-       xgboost,
-       scals,
-       purr,
-       glmnet) # Muestreo espacial para modelos de aprendizaje automático
-
-# cargar base de datos 
-
-bd <- read.csv('https://raw.githubusercontent.com/jbermudezc01/Problem_set2_BDML/main/stores/base_datos_tratada.csv')
-
-# crear subset de entrenamiento
-train <-  bd %>%
-  subset(type_data == 1)
-
-# crear subset de testeo
-test <- bd %>%
-  subset(type_data == 2)
-
-# modelo lasso 
-
-# especificación del modelo 
-
-lasso_spec <- linear_reg(penalty = tune(), mixture = 1) %>%
-  set_mode("regression") %>%
-  set_engine("glmnet") 
-
-# definir intervalo de parametros
-
-penalty_grid <- grid_regular(penalty(range = c(-2, 2)), levels= 100)
-
-# receta de preprocesamiento 
-
-receta <- recipe(formula = price ~ bathrooms+
-                   bedrooms+rooms+property_type+parqueadero+
-                   terraza+ascensor+deposito+vigilancia+cocina_integral+distancia_parques+
-                   distancia_estaciones_tp+distancia_centro_servicios, data = bd) %>%
-  
-  step_novel(all_nominal_predictors()) %>% 
-  step_dummy(all_nominal_predictors()) %>% 
-  step_zv(all_predictors()) 
-
-# crear flujo de trabajo 
-
-lasso_workflow <- workflow() %>%
-  add_recipe(receta) %>%
-  add_model(lasso_spec)
-
-
-# realizar busqueda de hiperparametros utilizando validación cruzada 
-
-# definir las particiones 
-
-df_fold <- vfold_cv(train, v = 5)
-
-# busqueda de hiperparametros
-
-tune_lasso <- tune_grid(
-lasso_workflow,
-resamples = df_fold, 
-grid = penalty_grid,
- metrics = metric_set(rmse)
-)
-
-# realizar bsuqeuda de hiperparametro 
+ # grafica de parametros 
 
 autoplot(tune_lasso)
 
+# seleccionar las mejores estimaciones de parametros
 
-lasso_fit <- fit(lasso_workflow, data = train)
-lasso_fit
+best_parms_lasso <- select_best(tune_lasso, metric = "rmse")
+best_parms_lasso
+
+# actulizar parametros con finalize_workflow() 
+
+lasso_final <- finalize_workflow(lasso_workflow, best_parms_lasso)
+
+#ajustar el modelo con los datos de test
+
+lasso_final_fit <- fit(lasso_final, data = train)
+
+# predecimos el precio para los datos de test 
+
+test <- test %>%
+  mutate(price = predict(lasso_final_fit, new_data = test)$.pred)
+
+# Exportar a CSV
+test %>% 
+  select(property_id, price) %>% 
+  write.csv(file = "01_lasso.csv", row.names = F)
+
