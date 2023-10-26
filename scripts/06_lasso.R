b8514ae3f5cedbec237c2a05f48d770e485eae00
Lina Maria Bautista Salcedo <l.bautistas@uniandes.edu.co>
2023-10-25 19:14:09 -0500

    nuevas configuraciones de lasso

--- scripts/06_lasso.R
+++ scripts/06_lasso.R
@@ -78,12 +78,6 @@ receta <- recipe(formula = log_price ~ ., data = bd.seleccion) %>%
   step_zv(all_predictors()) %>% 
   step_normalize(all_predictors())
 
-# receta <- recipe(formula = formula.lasso, data = bd) %>%
-#   step_novel(all_nominal_predictors()) %>% 
-#   step_dummy(all_nominal_predictors()) %>% 
-#   step_zv(all_predictors()) %>% 
-#   step_normalize(all_predictors())
-
 # Workflow ----------------------------------------------------------------
 lasso_workflow <- workflow() %>%
   add_recipe(receta) %>%
