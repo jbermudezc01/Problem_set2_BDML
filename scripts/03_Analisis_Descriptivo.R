ec0ce5a16ee1184644d656620cba3f20ea04be9c
jbermudezc01 <jbermudezc@unal.edu.co>
2023-10-28 15:25:58 -0500

    Arreglo del orden del repositorio. En carpeta views se creo 3 carpetas distintas, para graficos, mapas y tablas. Correccion de codigo 3 para ir acorde a la division de carpetas. Aparte, en un commit anterior no habia cargado bien la funcion para hacer los histogramas automaticamente

--- scripts/03_Analisis_Descriptivo.R
+++ scripts/03_Analisis_Descriptivo.R
@@ -25,7 +25,9 @@ p_load(tidyverse, # Manipular dataframes
        tmaptools,
        terra,
        purrr,
-       glmnet) 
+       glmnet,
+       webshot,
+       htmlwidgets) 
 
 # Directorios -------------------------------------------------------------
 stores    <- paste0(getwd(),'/stores/') # Directorio de base de datos
@@ -49,84 +51,38 @@ plot.precio <-bd %>%
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5, size = 16))
 # Guardar el grafico en la carpeta <Views>
-ggsave("histograma_precio.png", plot.precio, width = 8, height = 6, units = "in")
+ggsave(paste0(views,'graficas_descriptivas/histograma_precio.png'), plot.precio, width = 8, height = 6, units = "in")
 
-# Relación entre precios y el tipo de propiedad 
-ggplot(bd, aes(x = price))+
-  geom_freqpoly(aes(color = property_type), binwidth = 5000, linewidth = 0.75) +
-  scale_x_continuous(labels = scales::dollar_format()) 
+# Histogramas distancia ---------------------------------------------------
+variables.distancia <- colnames(bd)[grepl('distancia',colnames(bd))]
+histogramas.distancia <- lapply(variables.distancia, function(name){
+  histograma.distancia <- bd %>% 
+    ggplot(aes(x=!!sym(name))) +
+    geom_histogram(fill='darkblue', alpha=0.5, color='black') +
+    labs(x= paste0(str_to_title(unlist(str_split(name,'_'))),collapse=' '),
+         y = 'Frecuencia') +
+    ggtitle(paste0('Histograma de ', paste0(str_to_title(unlist(str_split(name,'_'))),collapse=' '))) +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5, size =16))
+  ggsave(paste0(views,'graficas_descriptivas/histograma_',name,'.png'), histograma.distancia, width = 8, height = 6 , units='in')
+})
 
-# Relación del precio y el número de habitaciones 
-ggplot(bd, aes(x = rooms, y = price)) + 
-  geom_boxplot(aes(group = cut_width(rooms, 0.1)))
-
-# Relación del precio y el número de baños 
-ggplot(bd, aes(x = bathrooms, y = price)) + 
-  geom_boxplot(aes(group = cut_width(bathrooms, 0.1)))
-
-# Visualizar localizacion variables OSM -----------------------------------
-
-
+# Graficas mapas ----------------------------------------------------------
 # Encontrammos el queremos que sea el centro del mapa  . bd es la base principal
-latitud_central  <- mean(bd$lat)
-longitud_central <- mean(bd$lon)
+raw.csv <- read_csv(paste0(stores,"raw_complete_base.csv"))
+latitud_central  <- mean(raw.csv$lat)
+longitud_central <- mean(raw.csv$lon)
 
-# GRAFICAR CORRECTAMENTE , iterando a traves de geometria.osm que tiene los poligonos, y coordenadas.x.centroides
+# Iterar a traves de geometria.osm que tiene los poligonos, y coordenadas.x.centroides
 # con coordenadas.y.centroides que tienen las coordenadas de centroides
-leaflet() %>%
+plot <- leaflet() %>%
   addTiles() %>%
-  setView(lng = longitud_central, lat = latitud_central, zoom = 12) %>%
+  setView(lng = longitud_central, lat = latitud_central, zoom = 11.6) %>%
   addPolygons(data = geometria.osm[[3]], col = "red",weight = 10,
               opacity = 0.8, popup = geometria.osm[[1]]$name) %>%
   addCircles(lng = coordenadas.x.centroides[[3]], 
              lat = coordenadas.y.centroides[[3]], 
              col = "darkblue", opacity = 0.5, radius = 1)
-
-
-# Grafica variables distancias --------------------------------------------
-# Distribución de la variable 
-
-#restaurante
-distancia_restaurantes <- ggplot(bd, aes(x = distancia_restaurante)) +
-  geom_histogram(bins = 50, fill = "darkblue", alpha = 0.4) +
-  labs(x = "Distancia mínima a un restaurante en metros", y = "Cantidad",
-       title = "Distribución de la distancia a restaurantes") +
-  theme_bw()
-ggplotly(distancia_restaurantes)
-#parques
-distancia_parques <- ggplot(bd, aes(x = distancia_parques)) +
-  geom_histogram(bins = 50, fill = "darkblue", alpha = 0.4) +
-  labs(x = "Distancia mínima a un parque en metros", y = "Cantidad",
-       title = "Distribución de la distancia a parques") +
-  theme_bw()
-ggplotly(distancia_parques)
-#estaciones
-distancia_estaciones <- ggplot(bd, aes(x = distancia_estaciones_tp)) +
-  geom_histogram(bins = 50, fill = "darkblue", alpha = 0.4) +
-  labs(x = "Distancia mínima a una estación de transporte público en metros", y = "Cantidad",
-       title = "Distribución de la distancia a una estación de transporte público") +
-  theme_bw()
-ggplotly(distancia_estaciones)
-#mall
-distancia_mall <- ggplot(bd, aes(x = distancia_mall)) +
-  geom_histogram(bins = 50, fill = "darkblue", alpha = 0.4) +
-  labs(x = "Distancia mínima a un centro comercial en metros", y = "Cantidad",
-       title = "Distribución de la distancia a un centro comercial") +
-  theme_bw()
-ggplotly(distancia_mall)
-#ciclovias
-distancia_ciclovia <- ggplot(bd, aes(x = distancia_ciclovias)) +
-  geom_histogram(bins = 50, fill = "darkblue", alpha = 0.4) +
-  labs(x = "Distancia mínima a una ciclovia en metros", y = "Cantidad",
-       title = "Distribución de la distancia a una ciclovia") +
-  theme_bw()
-ggplotly(distancia_ciclovia)
-# centro servicios
-distancia_servicios<- ggplot(bd, aes(x = distancia_centro_servicios)) +
-  geom_histogram(bins = 50, fill = "darkblue", alpha = 0.4) +
-  labs(x = "Distancia mínima a centros de servicio en metros", y = "Cantidad",
-       title = "Distribución de la distancia a centro de servicios") +
-  theme_bw()
-ggplotly(distancia_servicios)
-
-
+# Guardar el archivo
+saveWidget(plot, paste0(views,'html_temp/plot.html'))
+webshot(paste0(views,'html_temp/plot.html'), file = "leaflet_map.png",zoom = 2)
